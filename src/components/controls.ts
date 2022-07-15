62ac70f1f7c9b45904a364e071e20fa568bac49d
franciscoJefersonDev <francisco.jeferson.dev@gmail.com>
2022-07-15 16:28:03 +0000

    hdhdd

--- src/components/controls.ts
+++ src/components/controls.ts
@@ -7,27 +7,36 @@ const component = `
       </button>
     </nav>
     <div class="content">
-      <div class="container-2">
-        <div class="col-1"></div>
-        <div class="swiper mySwiper">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide">Slide 1</div>
-            <div class="swiper-slide">Slide 2</div>
-            <div class="swiper-slide">Slide 3</div>
-            <div class="swiper-slide">Slide 4</div>
-            <div class="swiper-slide">Slide 5</div>
-            <div class="swiper-slide">Slide 6</div>
-            <div class="swiper-slide">Slide 7</div>
-            <div class="swiper-slide">Slide 8</div>
-            <div class="swiper-slide">Slide 9</div>
-          </div>
-          <div class="swiper-pagination"></div>
-        </div>
+      <div class="container-1">
+        <div class="c-col">a</div>
+        <div class="c-col">a</div>
+        <div class="c-col">a</div>
       </div>
     </div>
   </div>
 </div>
 `
+
+/*
+<div class="container-2">
+  <div class="col-1"></div>
+  <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide">Slide 4</div>
+      <div class="swiper-slide">Slide 5</div>
+      <div class="swiper-slide">Slide 6</div>
+      <div class="swiper-slide">Slide 7</div>
+      <div class="swiper-slide">Slide 8</div>
+      <div class="swiper-slide">Slide 9</div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</div>
+*/
+
 export default (() => {
   const app = document.querySelector<HTMLDivElement>('#app')!
   app.innerHTML += component
